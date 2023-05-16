24de4fdc571ae8b124b417b30cc75ef99b6a6bf4
+ <+>
2023-05-16 15:56:15 +0300

    Complete

--- app.rb
+++ app.rb
@@ -1,3 +1,14 @@
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
+
+puts "Hello world!"
+p 'Hello world'
+print "Hello world!"
+
+
 # pp [{ id: 1, hello: "World" }, 
 #     { id: 2, hello: "Ruby" }, 
 #     { id: 3, hello: "Moon" }, 
